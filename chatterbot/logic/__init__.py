4225050e02e90a56daa31b5140cc78d51d204eeb
soyeon <soyeon@soyeonui-MacBookPro.local>
2020-06-15 23:02:21 +0900

    delete temperatur_adapter

--- chatterbot/logic/__init__.py
+++ chatterbot/logic/__init__.py
@@ -4,7 +4,6 @@ from chatterbot.logic.mathematical_evaluation import MathematicalEvaluation
 from chatterbot.logic.specific_response import SpecificResponseAdapter
 from chatterbot.logic.time_adapter import TimeLogicAdapter
 from chatterbot.logic.unit_conversion import UnitConversion
-from chatterbot.logic.temperature_adapter import TemperatureAdapter
 from chatterbot.logic.retrieval_custom import RetrievalCustom
 from chatterbot.logic.parlai_adapter import ParlAI
 
@@ -14,7 +13,6 @@ __all__ = (
     'MathematicalEvaluation',
     'SpecificResponseAdapter',
     'TimeLogicAdapter',
-    'TemperatureAdapter',
     'UnitConversion',
     'RetrievalCustom',
     'ParlAI'
